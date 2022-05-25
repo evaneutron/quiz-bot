e41e1394a1c4e332709170dfda326bc64b636e92
Olegic <o.zaitsev@gmail.nsu.ru>
2022-05-25 17:22:21 +0700

    done

--- table.py
+++ table.py
@@ -19,7 +19,6 @@ class Table:
         for i in range(rows_cnt):
             self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spread_sheet_id, body={
                 "valueInputOption": "USER_ENTERED",
-                # Данные воспринимаются, как вводимые пользователем (считается значение формул)
                 "data": [
                     {"range": "Лист номер один!{}2".format(next_sym('B', i)),
                     "values": [
@@ -31,7 +30,6 @@ class Table:
     def add_user(self, user_name, index):
         self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spread_sheet_id, body={
             "valueInputOption": "USER_ENTERED",
-            # Данные воспринимаются, как вводимые пользователем (считается значение формул)
             "data": [
                 {"range": "Лист номер один!A{}".format(index + 3),
                 "values": [
@@ -43,7 +41,6 @@ class Table:
     def add_answer(self, answer, player_index, question_num):
         self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spread_sheet_id, body={
             "valueInputOption": "USER_ENTERED",
-            # Данные воспринимаются, как вводимые пользователем (считается значение формул)
             "data": [
                 {"range": "Лист номер один!{}{}".format(next_sym('A', question_num - 1), player_index + 3),
                 "values": [
